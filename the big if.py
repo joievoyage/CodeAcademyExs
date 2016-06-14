33f0f4d754e2b5f15527942a12e117b60ade4ca7
Joie <jwcubefamily@gmail.com>
2016-06-14 18:48:36 +0800

    Add files via upload

---
+++ the big if.py
@@ -0,0 +1,20 @@
+# Make sure that the_flying_circus() returns True
+
+def the_flying_circus():
+    a = raw_input() ("enter your age")
+    if (a >= 18 and a<=21):    # Start coding here!
+        return True
+    	print "you're young af"
+        # Don't forget to indent
+        # the code inside this block!
+    elif (a >= 21):
+        print "yeah youre old enough"
+        return True
+        # Keep going here.
+        # You'll want to add the else statement, too!
+    elif (a >= 21):
+        print "yeah youre too young"
+        return True
+    else:
+        print "you have entered invalid text"
+        return True
