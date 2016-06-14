49e342a7a8ee04968e673c15e88562ca86f86f14
Joie <jwcubefamily@gmail.com>
2016-06-14 19:51:45 +0800

    Add files via upload

---
+++ pyglatin.py
@@ -0,0 +1,13 @@
+print 'Welcome to the Pig Latin Translator!'
+
+# Start coding here!
+original = raw_input("Enter a word:")
+
+if len(original) > 0:
+    print original
+    #Run this block.
+    #Maybe print something?
+
+else:
+    print "This is empty."
+    #That string must be original.
