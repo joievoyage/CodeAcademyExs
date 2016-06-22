0d1a326bbd6f554fb8d22db2406cb309b8603fbd
Joie <jwcubefamily@gmail.com>
2016-06-22 16:40:01 +0800

    Add files via upload

---
+++ lists_functions.py
@@ -0,0 +1,10 @@
+# Write your function below!
+def fizz_count(x):
+    count = 0
+    for item in x:
+    # Do remember to put == inside if
+        if item == 'fizz':
+            count = count + 1
+    return count
+
+    
